8b0712fdc8950f193e2840647b27036f3e58d661
Articfox <59283331+SamsRepo123@users.noreply.github.com>
2021-02-16 16:05:21 +0530

    Social Networking App Chat App -Splash Activity (Functional with logic of Setup Profile)

--- ChatApp/app/src/main/java/com/example/chatapp/SplashActivity.java
+++ ChatApp/app/src/main/java/com/example/chatapp/SplashActivity.java
@@ -1,30 +1,76 @@
 package com.example.chatapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.WindowManager;
 
-public class SplashActivity extends AppCompatActivity {
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
+public class SplashActivity extends AppCompatActivity {
+    FirebaseAuth mAuth;
+    DatabaseReference mRef;
+    FirebaseUser mUser;
+    Toolbar toolbar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        toolbar = findViewById(R.id.app_bar);
+        setSupportActionBar(toolbar);
+        mAuth = FirebaseAuth.getInstance();
+        mUser = mAuth.getCurrentUser();
+        mRef = FirebaseDatabase.getInstance().getReference().child("Users");
         setContentView(R.layout.activity_splash); getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         Runnable runnable = new Runnable() {
-            @Override
-            public void run() {
-                Intent intent = new Intent(SplashActivity.this, LoginActivity.class);
-                intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
-                startActivity(intent);
-                finish();
+                @Override
+                public void run() {
+                    if (mUser !=null){
+                        mRef.child(mUser.getUid()).addValueEventListener(new ValueEventListener() {
+                            @Override
+                            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                                if(dataSnapshot.exists()){
+                                    Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+                                    startActivity(intent);
+                                    finish();
+                                }
+                                else{
+                                    Intent intent = new Intent(SplashActivity.this, SetupActivity.class);
+                                    startActivity(intent);
+                                    finish();
+                                }
+                            }
 
-            }
+                            @Override
+                            public void onCancelled(@NonNull DatabaseError databaseError) {
 
+                            }
+                        });
+                    }
+                    else {
+                        Intent intent = new Intent(SplashActivity.this, LoginActivity.class);
+                        startActivity(intent);
+                        finish();
+                    }
+                }
             };
-        Handler handler = new Handler();
+            Handler handler = new Handler();
         handler.postDelayed(runnable,5000);
-    }
-}
+        }
+
+        public void setSupportActionBar(Toolbar toolbar) {
+        }
+
+        }
+
